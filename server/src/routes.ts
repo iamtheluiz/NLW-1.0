c098b6b58a7548ee3602b3b5bfa64c3e3a608a99
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-06-06 17:37:25 -0300

    feat: add points list

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,7 +12,9 @@ const itemsController = new ItemsController();
 
 // Routes
 routes.get('/items', itemsController.index);
-routes.get('/points/:id', pointsController.show)
+
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
