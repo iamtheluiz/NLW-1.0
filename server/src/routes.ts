7ba0ff8439d46e31bbfcdc373f202a7ebc6bd9b9
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-06-06 17:02:50 -0300

    feat: list items and create point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,57 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import knex from './database/connection';
 
 // Define routes
 const routes = express.Router();
 
-routes.get('/', (req, res): Response<{ message: string }> => {
+routes.get('/items', async (req, res) => {
+  // Get items
+  const items = await knex('items').select('*');
+
+  // Format
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  // Return
+  return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+  // Get new point data
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body;
+
+  // Transaction
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake.jpg',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    };
+  });
+
+  await trx('point_items').insert(pointItems);
+
   return res.json({
-    message: 'Hello World'
+    success: true
   });
 });
 
